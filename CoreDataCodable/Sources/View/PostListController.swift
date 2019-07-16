c3ba038bc1416b946c482b2f25a2eae5568abb47
emile <emile@blueantcorp.com>
2019-07-16 15:04:31 +0300

    Add views

--- CoreDataCodable/Sources/View/PostListController.swift
+++ CoreDataCodable/Sources/View/PostListController.swift
@@ -31,13 +31,6 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
-
+class PostListController: UITableViewController {
 
 }
-
